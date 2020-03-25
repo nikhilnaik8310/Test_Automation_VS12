19cb2d0d66fc99a4817f61e619b2ab4f81491e0d
Nikhil Naik <59816619+nikhilnaik8310@users.noreply.github.com>
2020-03-25 18:14:38 +0530

    Added Description

--- TA_VS12/Program.cs
+++ TA_VS12/Program.cs
@@ -12,20 +12,20 @@ namespace TA_VS12
         {
             try
             {
-                SerialPort S = new SerialPort("COM4", 9600);
-                S.Open();
+                SerialPort S = new SerialPort("COM4", 9600); //@ Step 1: //@@ Set Communication Port
+                S.Open(); //@Step2: //@@ Open Serial port 
                 Thread.Sleep(2000);
-                string s = S.ReadExisting();
-                if (s.Contains("Nikhil Naik"))
+                string s = S.ReadExisting(); //@Step 3: //@@ Read Serial port 
+                if (s.Contains("Nikhil Naik")) //@ Step 4: //@@ Verify Data
                 {
-                    Console.WriteLine("Pass");
+                    Console.WriteLine("PASS"); //@@ If Equel Pass.
                 }
                 else
                 {
-                    Console.WriteLine("Fail");
+                    Console.WriteLine("FAIL"); //@@ If Not Equel Fail.
                 }
                 Thread.Sleep(10000);
-                S.Close();
+                S.Close(); //@ Step 5: //@@ Close Serial Port.
             }
             catch (Exception ex)
             {
